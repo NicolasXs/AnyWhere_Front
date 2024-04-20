28d891031fb23732cc1c4f30dd6adb340384be1f
GabryelKadmo <gabryelkadmo@gmail.com>
2024-04-19 23:10:38 -0300

    feat: login page is done

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,25 @@
+import imgRigt from "./img/loginPageRightSide.svg";
+import anywhereLogo from "./img/Anywhere.svg";
+
+import FormLogin from "./components/FormLogin";
+
 export default function LoginPage() {
-    return (
-        <div className="flex items-center justify-center">
-            <h1 className="text-9xl">Login Page</h1>
-        </div>
-    );
+  return (
+    <div className="flex">
+      <div className="h-screen w-screen  flex-col justify-start">
+        <img className="w-1/5 ml-10 mt-10" src={anywhereLogo} alt="logo" />
+        <p className="text-3xl mt-10 mb-20 text-customVermelho font-semibold text-center justify-start font-[inter]">
+          Estamos felizes em tê-lo de volta!
+          <span className="text-2xl inline-block text-black opacity-60 font-normal justify-start ">
+            Por favor, faça login para acessar sua conta.
+          </span>
+        </p>
+
+        <FormLogin />
+      </div>
+      <div className="bg-customCiano h-screen w-screen items-center justify-center hidden md:flex">
+        <img className="w-3/4" src={imgRigt} alt="imagem lado direito" />
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customCiano: "#DAFAFC",
+        customVermelho: "#FF3E30",
+        customAzul: "#176BEF",
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      }
+    },
+  },
+  fontFamily: {
+    'poppins': ['Poppins'],
+    'inter': ['Inter'],
   },
   plugins: [],
-}
+};
