af6180a42318d933c0d2cc7acf721299af372557
GabryelKadmo <gabryelkadmo@gmail.com>
2024-04-23 16:55:28 -0300

    feat: testing 1366x768

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -11,19 +11,29 @@ export default function LoginPage() {
         <p className="text-3xl mt-10 mb-20 text-customVermelho font-semibold text-center justify-start font-[inter] ">
           Estamos felizes em tê-lo de volta!
           <span
-            className={`text-3xl ${
-              window.innerWidth >= 2560 && window.innerWidth < 3440
-                ? "block"
-                : "inline-block"
-            } text-black opacity-60 font-normal justify-start `}
-          >
-            Por favor, faça login para acessar sua conta.
-          </span>
+  className={`${
+    (window.innerWidth >= 2560 && window.innerWidth < 3440) ||
+    (window.innerWidth === 1366 && window.innerHeight === 768)
+      ? "text-3xl block"
+      : "text-base sm:text-lg inline-block"
+  } ${
+    (window.innerWidth === 1366 && window.innerHeight === 768)
+      ? "text-sm"
+      : ""
+  } text-black opacity-60 font-normal justify-start ${
+    window.innerWidth < 1366 ? "" : ""
+  }`}
+>
+  Por favor, faça login para acessar sua conta.
+</span>
+
         </p>
 
         <FormLogin />
       </div>
-      <div className="bg-customCiano h-screen w-screen items-center justify-center hidden md:flex">
+      <div
+        className={`bg-customCiano h-screen  w-screen items-center justify-center hidden md:flex ${""}`}
+      >
         <img className="w-3/4" src={imgRigt} alt="imagem lado direito" />
       </div>
     </div>
