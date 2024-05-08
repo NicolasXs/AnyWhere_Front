6707f5dd030481608d2cdb317272d02164619dce
Sabrina <sabrinapscampos@gmail.com>
2024-05-07 22:00:01 -0300

    feat: adds payment form

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import PropertyRegister from "../pages/PropertyRegisterPage/PropertyRegister";
+import PaymentForm from "../pages/Checkout/PaymentForm";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/cadastrar-propriedade" element={<PropertyRegister />} />
+      <Route path="/payment" element={<PaymentForm />} />
     </Routes>
   );
 }
